09953f485aab743c5b89f36722c08a1e4298c2ef
Patrick A <akolo@hotmail.com>
2020-04-15 23:14:38 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,15 +1,15 @@
 require 'pry'
 
-def engage
-  puts state_log(date)
-  date = generate_star_date
-  binding.pry
-end
-
 def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
 
 def state_log(date)
   "Captain's Log, star date #{date}."
+end
+
+def engage
+  puts state_log(date)
+  date = generate_star_date
+  binding.pry
 end
